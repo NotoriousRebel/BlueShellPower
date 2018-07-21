c9acb4e5781e1c4b212f09bec1a3ac976797d5f8
Matt <36310667+NotoriousRebel@users.noreply.github.com>
2018-07-21 18:41:05 -0400

    Add files via upload
    Two scripts that stop all executables without crashing your system and the other script goes through all users and disables them making sure to not disable the current user.

---
+++ Blue/fixusers.ps1
@@ -0,0 +1,25 @@
+﻿$Accounts =  Get-WmiObject -Class Win32_UserAccount -filter "LocalAccount = True"
+$ListUsers = @()
+$currentuser = $env:USERNAME
+$Accounts = $Accounts -split ' '
+
+ForEach($account in $Accounts){
+         $stringAccount = [string]$account -split '"'
+         for($i = 0; $i -lt $stringAccount.Count; $i+=1){
+             if ($i -eq 3){
+             $user = $stringAccount[$i]
+             $ListUsers += $user
+             }
+         }
+}
+
+ForEach($user in $ListUsers){
+      if (-not($username -eq $currentuser)){
+            Try{
+                Disable-LocalUser -Name $username
+            }
+            Catch{
+                continue
+            }
+        }
+}
---
+++ Blue/stopexes.ps1
@@ -0,0 +1,20 @@
+﻿$tasklist = tasklist.exe
+$tasklist = $tasklist.Split(" ") 
+$truetaskList =  @()
+
+ForEach($task in $tasklist){
+    if ($task -match '.exe' -and -Not($truetaskList.Contains($task)) -and -Not($task -match 'powershell')){
+        $truetaskList += $task
+    }
+}
+
+
+ForEach($task in $truetaskList){
+    Try{
+            $truetask = $task.Substring(0,$task.Length-4)
+            Stop-Process -Name $truetask 
+    }
+    Catch{
+   continue 
+    }
+}
