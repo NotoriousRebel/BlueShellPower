d8539aa032493040ca8340e672ed8183aed968bd
Matt <36310667+NotoriousRebel@users.noreply.github.com>
2018-07-21 18:47:02 -0400

    Add files via upload
    Tries to block port specified by user.

---
+++ Blue/block_port.ps1
@@ -0,0 +1,9 @@
+﻿$num = Read-Host 'Enter Port to Block'
+Try{
+    netsh advfirewall firewall add rule name="Blocktcp + $num" protocol=TCP dir=in localport = $num action=block
+    netsh advfirewall firewall add rule name="Blockudp + $num" protocol=TCP dir=out localport = $num action=block
+    #echo $num
+    }
+    Catch{
+        	echo "Blocking failed" 
+	}
